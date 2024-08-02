5a06e4ad09b2ebec465aa65a1c7bf717354727a5
vallliere <163427872+vallliere@users.noreply.github.com>
2024-08-03 08:06:12 +0900

    Update pbds.cpp

--- pbds.cpp
+++ pbds.cpp
@@ -1,4 +1,5 @@
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+#define ordered_set(t) tree<t, null_type, less<t>, rb_tree_tag, tree_order_statistics_node_update>
