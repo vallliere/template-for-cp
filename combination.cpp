16121326944cc150f8f246e62de2968bc1becc90
vallliere <163427872+vallliere@users.noreply.github.com>
2024-08-29 16:32:27 +0900

    Update combination.cpp

--- combination.cpp
+++ combination.cpp
@@ -7,42 +7,38 @@ struct combination {
         fac[0] = ifac[0] = 1;
         for (int i = 1; i < fac.size(); i++)
             fac[i] = fac[i - 1] * i % md;
-        ifac.back() = power(fac.back(), md - 2, md);
+        ifac.back() = power(fac.back(), md - 2);
         for (int i = ifac.size() - 2; i > 0; i--)
             ifac[i] = ifac[i + 1] * (i + 1) % md;
+    }
+    ll power(ll a, ll b)
+    {
+        ll ret;
+        ret = 1;
+        while (b) {
+            if (b & 1)
+                ret = ret * a % md;
+            a = a * a % md;
+            b >>= 1;
+        }
+        return ret;
     }
     ll power(ll a, ll b, ll _md)
     {
         ll ret;
         ret = 1;
-        while (b > 0) {
-            if (b % 2 == 1)
+        while (b) {
+            if (b & 1)
                 ret = ret * a % _md;
             a = a * a % _md;
-            b /= 2;
+            b >>= 1;
         }
         return ret;
     }
-    ll nCr(ll a, ll b)
-    {
-        ll ret;
-        ret = fac[a];
-        ret = ret * ifac[b] % md;
-        ret = ret * ifac[a - b] % md;
-        return ret;
-    }
-    ll nHr(ll a, ll b)
-    {
-        return nCr(a + b - 1, b);
-    }
-    ll factorial(ll a)
-    {
-        return fac[a];
-    }
-    ll inverse_factorial(ll a)
-    {
-        return ifac[a];
-    }
+    ll nCr(ll a, ll b) { return fac[a] * ifac[b] % md * ifac[a - b] % md; }
+    ll nHr(ll a, ll b) { return nCr(a + b - 1, b); }
+    ll factorial(ll a) { return fac[a]; };
+    ll inverse_factorial(ll a) { return ifac[a]; }
 
    private:
     ll md;
