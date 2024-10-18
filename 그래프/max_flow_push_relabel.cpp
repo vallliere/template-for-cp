3d9182c8b1c281deb94576620dcfb58d4effbf39
vallliere <163427872+vallliere@users.noreply.github.com>
2024-10-18 17:42:55 +0900

    Update max_flow_push_relabel.cpp

--- 그래프/max_flow_push_relabel.cpp
+++ 그래프/max_flow_push_relabel.cpp
@@ -10,13 +10,9 @@ struct push_relabel {
         N = _N;
         S = T = -1;
         grp.resize(N);
-        stk.resize(N * 2);
     }
     void add_edge(int u, int v, flow_t flow)
     {
-        assert(flow >= 0);
-        assert(u >= 0 && u < N && v >= 0 && v < N);
-        assert(u != v);
         grp[u].push_back({v, flow, int(grp[v].size())});
         grp[v].push_back({u, 0, int(grp[u].size()) - 1});
     }
@@ -24,46 +20,56 @@ struct push_relabel {
     {
         S = _S, T = _T;
     }
-    void add_flow(edge &lo, flow_t va)
-    {
-        if (ex[lo.v] == 0 && va > 0)
-            stk[hei[lo.v]].push_back(lo.v);
-        lo.flw -= va;
-        grp[lo.v][lo.rev].flw += va;
-        ex[grp[lo.v][lo.rev].v] -= va;
-        ex[lo.v] += va;
-    }
 
-    // max flow
+    // max_flow
     flow_t max_flow()
     {
-        int u, hi;
         assert(S != -1 && T != -1);
-        ex.assign(N, 0);
-        idx.assign(N, 0);
-        hei.assign(N, 0);
+
+        vector<int> idx(N);
+        vector<int> hei(N, 0);
+        vector<int> cnt(N * 2);
+        vector<flow_t> ex(N);
+        vector<vector<int>> stk(N * 2);
+
+        function<void(edge &, flow_t)> push = [&](edge &lo, flow_t va) {
+            if (ex[lo.v] == 0 && va > 0)
+                stk[hei[lo.v]].push_back(lo.v);
+            lo.flw -= va;
+            grp[lo.v][lo.rev].flw += va;
+            ex[grp[lo.v][lo.rev].v] -= va;
+            ex[lo.v] += va;
+        };
+
+        function<void(int)> relabel = [&](int u) {
+            hei[u] = 3 * N;
+            for (int i = 0; i < grp[u].size(); i++) {
+                edge &lo = grp[u][i];
+                if (lo.flw > 0 && hei[lo.v] + 1 < hei[u]) {
+                    hei[u] = hei[lo.v] + 1;
+                    idx[u] = i;
+                }
+            }
+        };
+
         hei[S] = N;
         ex[T] = 1;
-        vector<int> cnt(N * 2);
         cnt[0] = N - 1;
-        for (auto &ne : grp[S])
-            add_flow(ne, ne.flw);
+        for (edge &ne : grp[S])
+            push(ne, ne.flw);
         if (stk[0].size() > 0) {
-            hi = 0;
+            int hi = 0;
             while (hi >= 0) {
-                u = stk[hi].back();
+                int u = stk[hi].back();
                 stk[hi].pop_back();
-                while (ex[u] > 0) {  // discharge u
+                // discharge u
+                while (ex[u] > 0) {
                     if (idx[u] == grp[u].size()) {
-                        hei[u] = 3 * N;
-                        for (int i = 0; i < grp[u].size(); i++) {  // relabel
-                            auto &lo = grp[u][i];
-                            if (lo.flw > 0 && hei[u] > hei[lo.v] + 1)
-                                hei[u] = hei[lo.v] + 1, idx[u] = i;
-                        }
+                        relabel(u);
                         cnt[hei[u]]++, cnt[hi]--;
-                        if (cnt[hi] == 0 && hi < N)  // gap heuristic
-                            for (int i = 0; i < N; ++i)
+                        // gap heuristic
+                        if (cnt[hi] == 0 && hi < N)
+                            for (int i = 0; i < N; i++)
                                 if (hei[i] > hi && hei[i] < N) {
                                     cnt[hei[i]]--;
                                     stk[hei[i]].clear();
@@ -71,10 +77,15 @@ struct push_relabel {
                                 }
                         hi = hei[u];
                     }
-                    else if (grp[u][idx[u]].flw > 0 && hei[u] == hei[grp[u][idx[u]].v] + 1)  // push
-                        add_flow(grp[u][idx[u]], min(ex[u], grp[u][idx[u]].flw));
-                    else
+                    while (idx[u] < grp[u].size()) {
+                        edge &lo = grp[u][idx[u]];
+                        if (lo.flw > 0 && hei[u] == hei[lo.v] + 1) {
+                            push(lo, min(ex[u], lo.flw));
+                            if (ex[u] == 0)
+                                break;
+                        }
                         idx[u]++;
+                    }
                 }
                 while (hi >= 0 && stk[hi].size() == 0)
                     hi--;
@@ -86,7 +97,4 @@ struct push_relabel {
    private:
     int N, S, T;
     vector<vector<edge>> grp;
-    vector<int> idx, hei;
-    vector<vector<int>> stk;
-    vector<flow_t> ex;
 };
