54eebe3adcf09f2a7b848e31fe5667e93cdb5a5b
vallliere <163427872+vallliere@users.noreply.github.com>
2024-10-18 17:48:28 +0900

    Update dinic.cpp

--- 그래프/dinic.cpp
+++ 그래프/dinic.cpp
@@ -1,22 +1,20 @@
 template <typename flow_t>
 struct dinic {
     struct edge {
-        int v;
-        flow_t cap;
-        int rev;
-        edge(int _v, flow_t _cp, int _rev)
-            : v(_v), cap(_cp), rev(_rev) {}
+        int v, rev;
+        flow_t flw;
+        edge(int _v, flow_t _flw, int _rev) : v(_v), flw(_flw), rev(_rev) {}
     };
-    dinic(int _sz)
+    dinic(int _N)
     {
-        sz = _sz;
-        idx.resize(sz);
-        lev.resize(sz);
-        grp.resize(sz);
+        N = _N;
+        idx.resize(N);
+        lev.resize(N);
+        grp.resize(N);
     }
-    void add_edge(int u, int v, flow_t cap)
+    void add_edge(int u, int v, flow_t flow)
     {
-        grp[u].push_back({v, cap, int(grp[v].size())});
+        grp[u].push_back({v, flow, int(grp[v].size())});
         grp[v].push_back({u, 0, int(grp[u].size()) - 1});
     }
     void set_ST(int s, int t)
@@ -25,36 +23,34 @@ struct dinic {
     }
     flow_t dfs(int lo, flow_t flw)
     {
-        flow_t ret;
+        flow_t ret_flow;
         if (lo == T)
             return flw;
         for (; idx[lo] < grp[lo].size(); idx[lo]++) {
             auto& ne = grp[lo][idx[lo]];
-            if (lev[ne.v] == lev[lo] + 1 && ne.cap > 0) {
-                ret = dfs(ne.v, min(flw, ne.cap));
-                if (ret > 0) {
-                    ne.cap -= ret;
-                    grp[ne.v][ne.rev].cap += ret;
-                    return ret;
+            if (lev[ne.v] == lev[lo] + 1 && ne.flw > 0) {
+                ret_flow = dfs(ne.v, min(flw, ne.flw));
+                if (ret_flow > 0) {
+                    ne.flw -= ret_flow;
+                    grp[ne.v][ne.rev].flw += ret_flow;
+                    return ret_flow;
                 }
             }
         }
         return 0;
     }
-    flow_t run()
+    flow_t get_flow(flow_t flow)
     {
-        int lo;
-        flow_t va, ret;
         fill(idx.begin(), idx.end(), 0);
         fill(lev.begin(), lev.end(), -1);
         queue<int> que;
         que.push(S);
         lev[S] = 1;
         while (que.size() > 0) {
-            lo = que.front();
+            int lo = que.front();
             que.pop();
             for (auto& ne : grp[lo]) {
-                if (lev[ne.v] == -1 && ne.cap > 0) {
+                if (lev[ne.v] == -1 && ne.flw > 0) {
                     que.push(ne.v);
                     lev[ne.v] = lev[lo] + 1;
                 }
@@ -62,30 +58,31 @@ struct dinic {
         }
         if (lev[T] == -1)
             return -1;
-        ret = 0;
-        for (;;) {
-            va = dfs(S, numeric_limits<flow_t>::max());
+        flow_t ret_flow = 0;
+        while (flow > 0) {
+            flow_t va = dfs(S, flow);
             if (va == 0)
                 break;
-            ret += va;
+            ret_flow += va;
+            flow -= va;
         }
-        return ret;
+        return ret_flow;
     }
-    flow_t complete_run()
+    flow_t max_flow()
     {
-        flow_t ret;
-        ret = 0;
+        flow_t ret_flow;
+        ret_flow = 0;
         for (;;) {
-            auto flw = run();
+            auto flw = get_flow(numeric_limits<flow_t>::max());
             if (flw == -1)
                 break;
-            ret += flw;
+            ret_flow += flw;
         }
-        return ret;
+        return ret_flow;
     }
 
    private:
-    int sz, S, T;
+    int N, S, T;
     vector<int> idx, lev;
     vector<vector<edge>> grp;
 };
