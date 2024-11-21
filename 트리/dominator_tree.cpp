30f7c2782b97be6e2544ede344e94d063c73f9e8
vallliere <163427872+vallliere@users.noreply.github.com>
2024-11-22 01:40:37 +0900

    Update dominator_tree.cpp

--- 트리/dominator_tree.cpp
+++ 트리/dominator_tree.cpp
@@ -1,17 +1,17 @@
 struct dominator_tree {
-    vector<vector<int>> g, rg, bucket;
+    vector<vector<int>> grp, rgrp, bucket;
     vector<int> arr, par, rev, sdom, dom, dsu, label;
     int N, T;
-    dominator_tree(int n) : g(n), rg(n), bucket(n), arr(n, -1), par(n, -1), rev(n, -1), sdom(n, -1), dom(n, -1), dsu(n, 0), label(n, 0), N(n), T(0) {}
-    void add_edge(int u, int v) { g[u].push_back(v); }
+    dominator_tree(int n) : grp(n), rgrp(n), bucket(n), arr(n, -1), par(n, -1), rev(n, -1), sdom(n, -1), dom(n, -1), dsu(n, 0), label(n, 0), N(n), T(0) {}
+    void add_edge(int u, int v) { grp[u].push_back(v); }
     void dfs(int u)
     {
         arr[u] = T, rev[T] = u, label[T] = sdom[T] = dsu[T] = T;
         T++;
-        for (int w : g[u]) {
+        for (int w : grp[u]) {
             if (arr[w] == -1)
                 dfs(w), par[arr[w]] = arr[u];
-            rg[arr[w]].push_back(arr[u]);
+            rgrp[arr[w]].push_back(arr[u]);
         }
     }
     int find(int u, int x = 0)
@@ -31,7 +31,7 @@ struct dominator_tree {
         dfs(root);
         iota(dom.begin(), dom.end(), 0);
         for (int i = T - 1; i >= 0; --i) {
-            for (int ne : rg[i])
+            for (int ne : rgrp[i])
                 sdom[i] = min(sdom[i], sdom[find(ne)]);
             if (i > 0)
                 bucket[sdom[i]].push_back(i);
@@ -48,9 +48,9 @@ struct dominator_tree {
         for (int i = 1; i < T; i++)
             if (dom[i] != sdom[i])
                 dom[i] = dom[dom[i]];
-        vector<int> outside_dom(N, -1);
+        vector<int> ret(N, -1);
         for (int i = 1; i < T; i++)
-            outside_dom[rev[i]] = rev[dom[i]];
-        return outside_dom;
+            ret[rev[i]] = rev[dom[i]];
+        return ret;
     }
 };
