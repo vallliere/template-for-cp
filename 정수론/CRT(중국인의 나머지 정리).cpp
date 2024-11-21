a56761b9fa6c2f43827e782206e4d0f6ade1a4c1
vallliere <163427872+vallliere@users.noreply.github.com>
2024-11-22 01:39:30 +0900

    Update CRT(중국인의 나머지 정리).cpp

--- 정수론/CRT(중국인의 나머지 정리).cpp
+++ 정수론/CRT(중국인의 나머지 정리).cpp
@@ -1,6 +1,5 @@
 array<ll, 3> crt(ll md1, ll va1, ll md2, ll va2)
-{
-    // ? = (va1 mod md1) and (va2 mod md2)
+{  // ? = (va1 mod md1) and (va2 mod md2)
     ll n, x, y;
     pil va;
     n = ((va2 - va1) % md2 + md2) % md2;
@@ -11,6 +10,5 @@ array<ll, 3> crt(ll md1, ll va1, ll md2, ll va2)
     va.second = md2 / gcd(md1, md2);
     va.first = (va.first % va.second + va.second) % va.second;
     x = va.second * md1, y = (va.first * md1 + va1) % x;
-    // answer = y mod x
-    return {1, x, y};
+    return {1, x, y};  // answer = y mod x
 }
