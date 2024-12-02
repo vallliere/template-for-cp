9e138a3c41639056bad82698832939582fc14cf6
vallliere <163427872+vallliere@users.noreply.github.com>
2024-12-02 18:49:35 +0900

    Delete 기하학/외심(circumcenter).cpp

--- 기하학/외심(circumcenter).cpp
+++
@@ -1,8 +0,0 @@
-pid cal_circumcenter(pid a, pid b, pid c)
-{
-    dl x, y, d;
-    d = (a.first * (b.second - c.second) + b.first * (c.second - a.second) + c.first * (a.second - b.second)) * 2;
-    x = (a.first * a.first + a.second * a.second) * (b.second - c.second) + (b.first * b.first + b.second + b.second) * (c.second - a.second) + (c.first * c.first + c.second * c.second) * (a.second - b.second);
-    y = (a.first * a.first + a.second * a.second) * (c.first - b.first) + (b.first * b.first + b.second + b.second) * (a.first - c.first) + (c.first * c.first + c.second * c.second) * (b.first - a.first);
-    return {x / d, y / d};
-}
