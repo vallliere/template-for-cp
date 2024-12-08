566e8e33f617cad7ed19c70cf858c231818d9160
vallliere <163427872+vallliere@users.noreply.github.com>
2024-12-08 16:03:18 +0900

    Update hash_map.cpp

--- 구현체/hash_map.cpp
+++ 구현체/hash_map.cpp
@@ -13,23 +13,23 @@ struct splitmix64_hash {
     typename enable_if<is_arithmetic<T>::value, size_t>::type
     operator()(T va) const
     {
-        static const uint64_t FIXED_RANDOM = splitmix64_hash_random(splitmix64_hash_seed);
-        return splitmix64(static_cast<uint64_t>(va) + FIXED_RANDOM);
+        static const uint64_t FIXED_RANDOM_1 = splitmix64_hash_random(splitmix64_hash_seed);
+        return splitmix64(static_cast<uint64_t>(va) + FIXED_RANDOM_1);
     }
 
     template <typename T>
     typename enable_if<!is_arithmetic<T>::value, size_t>::type
     operator()(const T &va) const
     {
-        static const uint64_t FIXED_RANDOM = splitmix64_hash_random(splitmix64_hash_seed);
-        uint64_t hash = FIXED_RANDOM;
+        static const uint64_t FIXED_RANDOM_2 = splitmix64_hash_random(splitmix64_hash_seed);
+        uint64_t hash = FIXED_RANDOM_2;
         for (const auto &ne : va)
             hash ^= splitmix64(static_cast<uint64_t>(ne) + hash);
         return hash;
     }
 
     static inline mt19937 splitmix64_hash_seed{static_cast<unsigned int>(chrono::steady_clock::now().time_since_epoch().count())};
-    static inline uniform_int_distribution<uint64_t> splitmix64_hash_random{static_cast<uint64_t>(1e8), static_cast<uint64_t>(1e12)};
+    static inline uniform_int_distribution<uint64_t> splitmix64_hash_random{static_cast<uint64_t>(1e8), static_cast<uint64_t>(1e8)};
 };
 template <typename type1, typename type2, typename hash_t = splitmix64_hash>
 using hash_map = __gnu_pbds::gp_hash_table<type1, type2, hash_t>;
